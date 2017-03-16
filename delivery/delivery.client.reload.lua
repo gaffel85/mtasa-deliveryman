c9e2e2b72d1d6cab1d58de5634321381befdae08
Ola Gawell <gaffel85@hotmail.com>
2017-03-16 07:50:41 +0100

    Fixed ereload time

--- delivery/delivery.client.reload.lua
+++ delivery/delivery.client.reload.lua
@@ -1,29 +1,30 @@
-local x = 1 rocketsFired = {}
-local magSize = 4;
+local rocketsFired = {}
+local magSize =2;
 local reloadTimeInMillis = 5000;
 
 function disableFireForHydra ( disable )
   if disable then
+    outputChatBox("Reloading")
     toggleControl ( "vehicle_secondary_fire", false ) -- disable their fire key
     toggleControl ( "vehicle_fire", true ) -- enable their fire key
   else
+    outputChatBox("Reloading done")
     toggleControl ( "vehicle_secondary_fire", true ) -- enable their fire key
     toggleControl ( "vehicle_fire", true ) -- enable their fire key
   end
 end
 
 function missileFired ()
-  outputDebugString("B")
   local now = getTickCount()
-  outputChatBox("Missile fired ".. now);
 
   -- Remove old rockets from history
   if #rocketsFired >= magSize then
     local done = false
     while not done do
-      local timeElapsed = now - rocketsFired[0];
+      local timeElapsed = now - rocketsFired[1];
+      outputDebugString("Duration: "..timeElapsed)
       if timeElapsed > reloadTimeInMillis then
-        table.remove(rocketsFired, 0)
+        table.remove(rocketsFired, 1)
         if #rocketsFired <= 0 then
           done = true
         end
@@ -34,16 +35,18 @@ function missileFired ()
   end
 
   if #rocketsFired < magSize then
-    table.insert(rocketsFired, time);
+    table.insert(rocketsFired, now);
   end
 
+  outputDebugString("fired: "..#rocketsFired .. " magSize: "  ..magSize)
   if #rocketsFired >= magSize then
     disableFireForHydra(true);
-    local reloadTime = getTickCount() - rocketsFired[0];
+    local reloadTime = reloadTimeInMillis - (getTickCount() - rocketsFired[1]);
+    outputDebugString("reloadTime: "..reloadTime)
     setTimer(function()
-      table.remove(rocketsFired, 0)
+      table.remove(rocketsFired, 1)
       disableFireForHydra(false);
-    end, reloadTime * 10, 1)
+    end, reloadTime, 1)
   end
 end
 
@@ -57,8 +60,8 @@ end
 addEventHandler( "onClientProjectileCreation", getRootElement(), projectileFired )
 
 function reloadTimeChangedRequest(newMagSize, newReloadTime)
-  magSize = newMagSize;
-  reloadTimeInMillis = newReloadTime;
+  magSize = tonumber(newMagSize);
+  reloadTimeInMillis = tonumber(newReloadTime);
 end
 addEvent("onReloadTimeChangedRequest", true)
 addEventHandler("onReloadTimeChangedRequest", getLocalPlayer(), reloadTimeChangedRequest)
